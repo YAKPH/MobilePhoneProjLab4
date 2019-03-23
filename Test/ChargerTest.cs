811a7f00d5961dae1c834631c6af533728156721
Yuliia Pykhnivska <YAK@simcorp.com>
2019-03-23 20:12:15 +0100

    with test

---
+++ Test/ChargerTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Chargers;
+using Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test
+{
+    [TestClass]
+    public class ChargerTest
+    {
+        [TestMethod]
+        public void CalculateChargingTimeCarZeroCap()
+        {
+            ICharger crg=ChargerFactory.GetCharger(ChargerTypes.Car);
+            double actRes=crg.CalculateChargingTime(0);
+            double expRes=0;
+            Assert.AreEqual(expRes, actRes);
+        }
+        [TestMethod]
+        public void CalculateChargingTimeWallZeroCap()
+        {
+            ICharger crg = ChargerFactory.GetCharger(ChargerTypes.Wall);
+            double actRes = crg.CalculateChargingTime(0);
+            double expRes = 0;
+            Assert.AreEqual(expRes, actRes);
+        }
+        [TestMethod]
+        public void CalculateChargingTimePowerBZeroCap()
+        {
+            ICharger crg = ChargerFactory.GetCharger(ChargerTypes.Powerbk);
+            double actRes = crg.CalculateChargingTime(0);
+            double expRes = 0;
+            Assert.AreEqual(expRes, actRes);
+        }
+    }
+}
---
+++ Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Test")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("325f96f6-4a57-47fa-b4e5-23281ab06527")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
