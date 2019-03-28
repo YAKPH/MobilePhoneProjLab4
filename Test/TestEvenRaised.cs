fe6396b989aa6f0d14fa4dfd2d3c570e29853fd6
Yuliia Pykhnivska <YAK@simcorp.com>
2019-03-28 16:46:55 +0100

    with Test of the even that is raised

---
+++ Test/TestEvenRaised.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PhoneLibrary;
+
+namespace Test
+{
+    [TestClass]
+    public class TestEvenRaised
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string expected ="Some message received";
+
+            string actual="";
+
+            SMSProvider smsProv = new SMSProvider();
+            smsProv.SMSReceived += delegate (string str)
+            {
+                actual = str.ToString();
+            };
+            smsProv.DoSMSReceived("Some message received");
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
